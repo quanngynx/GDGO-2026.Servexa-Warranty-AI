bd36a57f48553759df3c2e5203a358e55ab12d2b
quan3473 <nguyenminhquan042004@gmail.com>
2026-05-06 12:20:21 +0700

    .

--- servexa-warranty-ai/apps/server/src/middlewares/index.ts
+++ servexa-warranty-ai/apps/server/src/middlewares/index.ts
@@ -2,4 +2,3 @@ export * from "./error-middleware"
 export * from "./request-context.middleware"
 export * from './authenticate.middleware'
 export * from './refresh-authenticate.middleware'
-export * from './require-roles.middleware'
