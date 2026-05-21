c24780bf83fa60725c448eb469be408a615e97c2
quan3473 <nguyenminhquan042004@gmail.com>
2026-05-21 22:19:44 +0700

    feat: refactor central warehouse management with updated components, improved data handling, and enhanced user interface for better usability

--- servexa-warranty-ai/apps/web/src/features/(SYSTEM-ADMINISTRATION)/central-warehouse-management/components/data-table-row-actions.tsx
+++ servexa-warranty-ai/apps/web/src/features/(SYSTEM-ADMINISTRATION)/central-warehouse-management/components/data-table-row-actions.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@servexa-warranty-ai/ui/components/button";
+import { Button } from '@servexa-warranty-ai/ui/components/button'
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -6,57 +6,55 @@ import {
   DropdownMenuSeparator,
   DropdownMenuShortcut,
   DropdownMenuTrigger,
-} from "@servexa-warranty-ai/ui/components/dropdown-menu";
-import { type Row } from "@tanstack/react-table";
-import { MoreHorizontal, Trash2, UserPen } from "lucide-react";
-import { type User } from "../data/schema";
-import { useUsers } from "./central-warehouse-provider";
+} from '@servexa-warranty-ai/ui/components/dropdown-menu'
+import { type Row } from '@tanstack/react-table'
+import { MoreHorizontal, Trash2, Warehouse } from 'lucide-react'
+import { type TotalWarehouse } from '../data/schema'
+import { useCentralWarehouse } from './central-warehouse-provider'
 
 type DataTableRowActionsProps = {
-  row: Row<User>;
-};
+  row: Row<TotalWarehouse>
+}
 
 export function DataTableRowActions({ row }: DataTableRowActionsProps) {
-  const { setOpen, setCurrentRow } = useUsers();
+  const { setOpen, setCurrentRow } = useCentralWarehouse()
   return (
-    <>
-      <DropdownMenu modal={false}>
-        <DropdownMenuTrigger asChild>
-          <Button
-            variant="ghost"
-            className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
-          >
-            <MoreHorizontal className="h-4 w-4" />
-            <span className="sr-only">Open menu</span>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end" className="w-[160px]">
-          <DropdownMenuItem
-            onClick={() => {
-              setCurrentRow(row.original);
-              setOpen("edit");
-            }}
-          >
-            Edit
-            <DropdownMenuShortcut>
-              <UserPen size={16} />
-            </DropdownMenuShortcut>
-          </DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem
-            onClick={() => {
-              setCurrentRow(row.original);
-              setOpen("delete");
-            }}
-            className="text-red-500!"
-          >
-            Delete
-            <DropdownMenuShortcut>
-              <Trash2 size={16} />
-            </DropdownMenuShortcut>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </>
-  );
+    <DropdownMenu modal={false}>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant='ghost'
+          className='flex h-8 w-8 p-0 data-[state=open]:bg-muted'
+        >
+          <MoreHorizontal className='h-4 w-4' />
+          <span className='sr-only'>Open menu</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align='end' className='w-[160px]'>
+        <DropdownMenuItem
+          onClick={() => {
+            setCurrentRow(row.original)
+            setOpen('edit')
+          }}
+        >
+          Edit
+          <DropdownMenuShortcut>
+            <Warehouse size={16} />
+          </DropdownMenuShortcut>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          onClick={() => {
+            setCurrentRow(row.original)
+            setOpen('delete')
+          }}
+          className='text-red-500!'
+        >
+          Delete
+          <DropdownMenuShortcut>
+            <Trash2 size={16} />
+          </DropdownMenuShortcut>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
 }
